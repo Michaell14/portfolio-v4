b8ada92b0ab6a19580cab8111dba0def8a6a7ed1
Michael Li <limichael909@gmail.com>
2026-09-09 13:41:36 -0400

    Adjust footer flower size

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
                     <Header />
                     <Profile />
                     <Footer />
-                    <img src="home_ex/flower.webp" alt="ascii-animation" loading="lazy" className='absolute h-1/2 sm:h-[60%] w-auto bottom-0 right-0 z-[-1] lg:block' />
+                    <img src="home_ex/flower.webp" alt="ascii-animation" loading="lazy" className='absolute h-[37.5%] sm:h-[45%] w-auto bottom-0 right-0 z-[-1] lg:block' />
                 </div>
             </div >
         </>
